74497a31e2e7ff1cf3986737d14b8ba0008d1f4f
Mateusz Krawczynski <matikrawczpl@gmail.com>
2024-12-26 13:07:51 +0000

    formatting changes, add prompt for server
    TODO: more cleanly integrate server prompt with the other prompt

--- src/server.py
+++ src/server.py
@@ -1,15 +1,17 @@
 # Taken from: https://pytwitchapi.dev/en/stable/tutorial/user-auth-headless.html
 
 import asyncio
-from twitchAPI.twitch import Twitch
-from twitchAPI.oauth import UserAuthenticator
-from twitchAPI.type import AuthScope, TwitchAPIException
+
 from flask import Flask, redirect, request
+from twitchAPI.oauth import UserAuthenticator
+from twitchAPI.twitch import Twitch
+from twitchAPI.type import AuthScope, TwitchAPIException
+
+from user_prompt import server_prompt_loop
 from usersettings import UserSettings
 
-
 TARGET_SCOPE = [AuthScope.CHAT_EDIT, AuthScope.CHAT_READ]
-MY_URL = 'http://localhost:17563/login/confirm'
+MY_URL = "http://localhost:17563/login/confirm"
 
 
 app = Flask(__name__)
@@ -17,41 +19,51 @@ twitch: Twitch
 auth: UserAuthenticator
 
 
-@app.route('/login')
+@app.route("/login")
 def login():
     return redirect(auth.return_auth_url())
 
 
-@app.route('/login/confirm')
+@app.route("/login/confirm")
 async def login_confirm():
-    state = request.args.get('state')
+    state = request.args.get("state")
     if state != auth.state:
-        return 'Bad state', 401
-    code = request.args.get('code')
+        return "Bad state", 401
+    code = request.args.get("code")
     if code is None:
-        return 'Missing code', 400
+        return "Missing code", 400
     try:
         token, refresh = await auth.authenticate(user_token=code)
         await twitch.set_user_authentication(token, TARGET_SCOPE, refresh)
-    except TwitchAPIException as e:
-        return 'Failed to generate auth token', 400
-    return 'Sucessfully authenticated!'
+    except TwitchAPIException:
+        return "Failed to generate auth token", 400
+    return "Sucessfully authenticated!"
 
 
 async def twitch_setup():
     global twitch, auth
-    twitch = await Twitch(UserSettings.settings['App ID'], UserSettings.settings['App Secret'])
+    twitch = await Twitch(
+        UserSettings.settings["App ID"], UserSettings.settings["App Secret"]
+    )
     auth = UserAuthenticator(twitch, TARGET_SCOPE, url=MY_URL)
 
 
-if __name__ == '__main__':
-    user_settings = UserSettings()
-    if user_settings.settings['App ID'] == '':
-        user_settings.settings['App ID'] = input('Enter App ID (Do not expose): ')
-        user_settings.save_to_file()
-    if user_settings.settings['App Secret'] == '':
-        user_settings.settings['App Secret'] = input('Enter App Secret (Do not expose): ')
-        user_settings.save_to_file()
+def main():
+    us = UserSettings()
+    while us.settings["App ID"] == "" or us.settings["App Secret"] == "":
+        print("App ID or Secret not set, running setup\n")
+        server_prompt_loop()
 
-    asyncio.run(twitch_setup())
-    print('If no errors printed, server is running')
+    try:
+        asyncio.run(twitch_setup())
+        print("Server successfully started.")
+        exit()
+    except Exception as e:
+        print(e)
+        print("Something went wrong")
+        server_prompt_loop()
+        main()
+
+
+if __name__ == "__main__":
+    main()
--- src/user_prompt.py
+++ src/user_prompt.py
@@ -1,61 +1,64 @@
-from usersettings import UserSettings
 import os
+
+from usersettings import UserSettings
 
 
 def clear() -> None:
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 
 def print_options() -> None:
     settings = UserSettings().settings
     print(f'1. {'Set' if settings["App ID"] == '' else 'Change'} App ID')
     print(f'2. {'Set' if settings["App Secret"] == '' else 'Change'} App Secret')
-    if settings['Target Channel'] == '':
-        print('3. Set Target Channel')
+    if settings["Target Channel"] == "":
+        print("3. Set Target Channel")
     else:
         print(f'3. Change Target Channel ({settings["Target Channel"]})')
-    print(f'4. Toggle Excluded User (Currently Excluded: {settings['Excluded Users']})')
-    print(f'5. Toggle Console Logging (Currently {'Enabled' if settings['Logging'] else 'Disabled'})')
-    print(f'6. Change Padding (Currently {settings['Padding']})', end='\n\n')
-    print('r. Run Bot')
-    print('q. Quit', end='\n\n')
+    print(f"4. Toggle Excluded User (Currently Excluded: {list(settings['Excluded Users'])})")
+    print(
+        f"5. Toggle Console Logging (Currently {'Enabled' if settings['Logging'] else 'Disabled'})"
+    )
+    print(f"6. Change Padding (Currently {settings['Padding']})", end="\n\n")
+    print("r. Run Bot")
+    print("q. Quit", end="\n\n")
 
 
 def handle_option(option: str) -> None:
     clear()
-    print('Press enter to cancel')
+    print("Press enter to cancel")
 
     user_settings = UserSettings()
     settings = user_settings.settings.copy()
-    u: str = ''
+    u: str = ""
     match option:
-        case '1':
-            u = input('Enter App ID: ')
-            settings['App ID'] = u
-        case '2':
-            u = input('Enter App Secret: ')
-            settings['App Secret'] = u
-        case '3':
-            print(f'Current Target Channel: {settings['Target Channel']}')
-            u = input('New Target Channel: ')
-            settings['Target Channel'] = u
-        case '4':
-            print(f'Current Excluded Users: {settings['Excluded Users']}')
-            u = input('Enter User to Toggle: ')
-            if u in settings['Excluded Users']:
-                settings['Excluded Users'].remove(u)
-            elif u != '':
-                settings['Excluded Users'].add(u)
-        case '5':
-            settings['Logging'] = not settings['Logging']
-        case '6':
+        case "1":
+            u = input("Enter App ID: ")
+            settings["App ID"] = u
+        case "2":
+            u = input("Enter App Secret: ")
+            settings["App Secret"] = u
+        case "3":
+            print(f"Current Target Channel: {settings['Target Channel']}")
+            u = input("New Target Channel: ")
+            settings["Target Channel"] = u
+        case "4":
+            print(f"Current Excluded Users: {settings['Excluded Users']}")
+            u = input("Enter User to Toggle: ")
+            if u in settings["Excluded Users"]:
+                settings["Excluded Users"].remove(u)
+            elif u != "":
+                settings["Excluded Users"].add(u)
+        case "5":
+            settings["Logging"] = not settings["Logging"]
+        case "6":
             try:
-                u = int(input('Enter Padding: '))
-                settings['Padding'] = max(0, u)
+                u = int(input("Enter Padding: "))
+                settings["Padding"] = max(0, u)
             except ValueError:
                 return
 
-    if u == '' and option != '5':
+    if u == "" and option != "5":
         return
 
     user_settings.settings = settings
@@ -67,11 +70,52 @@ def prompt_loop() -> None:
         clear()
         print_options()
 
-        user_input = input('Enter option: ').lower()
-        if user_input == 'r':
+        user_input = input("Enter option: ").lower()
+        if user_input == "r":
             break
-        if user_input == 'q':
+        if user_input == "q":
             exit()
         handle_option(user_input)
 
     clear()
+
+
+def print_options_server() -> None:
+    settings = UserSettings().settings
+    print(f'1. {'Set' if settings["App ID"] == '' else 'Change'} App ID')
+    print(f'2. {'Set' if settings["App Secret"] == '' else 'Change'} App Secret\n')
+    print("r. Run server")
+    print("q. Quit\n")
+
+
+def handle_option_server(option: str) -> None:
+    clear()
+    print("Press enter to cancel")
+
+    user_settings = UserSettings()
+    settings = user_settings.settings.copy()
+    u: str = ""
+    match option:
+        case "1":
+            u = input("Enter App ID: ")
+            settings["App ID"] = u
+        case "2":
+            u = input("Enter App Secret: ")
+            settings["App Secret"] = u
+
+    if u == "":
+        return
+    user_settings.settings = settings
+    user_settings.save_to_file()
+
+
+def server_prompt_loop() -> None:
+    while True:
+        print_options_server()
+
+        user_input = input("Enter option: ").lower()
+        if user_input == "r":
+            break
+        if user_input == "q":
+            exit()
+        handle_option_server(user_input)
