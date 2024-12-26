abc9c62a64a89bb9d83c09642a9c31dbca7eb04f
Mateusz Krawczynski <matikrawczpl@gmail.com>
2024-12-26 13:07:17 +0000

    formatting changes, make file loc more platform compatible

--- src/usersettings.py
+++ src/usersettings.py
@@ -1,44 +1,46 @@
-import os
 import json
+import os
 import threading
+from pathlib import Path
 
 
 class UserSettings:
     _instance = None
     _lock = threading.Lock()
-    settings: dict
-    file_loc: os.path
+    settings: dict[str, str | set[str] | list[str] | bool | int]
+    file_loc: Path
 
-    def __new__(cls) -> 'UserSettings':
+    def __new__(cls) -> "UserSettings":
         with cls._lock:
             if cls._instance is None:
                 cls._instance = super(UserSettings, cls).__new__(cls)
                 cls.settings = {
-                    'App ID': '',
-                    'App Secret': '',
-                    'Target Channel': '',
-                    'Excluded Users': set(),
-                    'Logging': True,
-                    'Padding': 0
+                    "App ID": "",
+                    "App Secret": "",
+                    "Target Channel": "",
+                    "Excluded Users": set(),
+                    "Logging": True,
+                    "Padding": 0,
                 }
-                cls.file_loc = os.path.join(os.path.abspath(__file__), os.pardir, os.pardir, 'user_settings.json')
+                p = Path(__file__)
+                cls.file_loc = p.parents[1] / "user_settings.json"
                 cls.load_from_file(cls)
 
         return cls._instance
 
     def save_to_file(cls) -> None:
-        cls.settings['Excluded Users'] = list(cls.settings['Excluded Users'])
-        with open(cls.file_loc, 'w') as fp:
+        cls.settings["Excluded Users"] = list(cls.settings["Excluded Users"])
+        with open(cls.file_loc, "w") as fp:
             json.dump(cls.settings, fp, indent=4)
-        cls.settings['Excluded Users'] = set(cls.settings['Excluded Users'])
+        cls.settings["Excluded Users"] = set(cls.settings["Excluded Users"])
 
     def load_from_file(cls) -> None:
         if not os.path.exists(cls.file_loc):
             cls.save_to_file(cls)
 
-        with open(cls.file_loc, 'r') as fp:
+        with open(cls.file_loc, "r") as fp:
             try:
                 cls.settings.update(json.load(fp))
-                cls.settings['Excluded Users'] = set(cls.settings['Excluded Users'])
+                cls.settings["Excluded Users"] = set(cls.settings["Excluded Users"])
             except json.JSONDecodeError:
                 cls.save_to_file(cls)
