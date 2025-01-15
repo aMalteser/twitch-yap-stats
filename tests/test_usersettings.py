6f051e5332dafadf4dfa8519290f2b40983232c7
Mateusz Krawczynski <matikrawczpl@gmail.com>
2025-01-15 14:19:06 +0000

    adjust tests to `SettingsData` class

--- tests/test_usersettings.py
+++ tests/test_usersettings.py
@@ -33,24 +33,24 @@ class UserSettingsTest(unittest.TestCase):
 
     def test_singleton(self) -> None:
         us1 = UserSettings()
-        self.assertTrue(us1.settings["App ID"] == "")
+        self.assertTrue(us1.settings.app_id == "")
 
         us2 = UserSettings()
         self.assertEqual(us1, us2)
 
     def test_changed_settings_apply_to_all(self) -> None:
         us1 = UserSettings()
-        self.assertTrue(us1.settings["App ID"] == "")
+        self.assertTrue(us1.settings.app_id == "")
 
         us2 = UserSettings()
-        us1.settings["App ID"] = "123"
-        self.assertEqual(us2.settings["App ID"], "123")
+        us1.settings.app_id = "123"
+        self.assertEqual(us2.settings.app_id, "123")
 
     def test_save_to_file(self) -> None:
         us = UserSettings()
-        self.assertTrue(us.settings["App ID"] == "")
+        self.assertTrue(us.settings.app_id == "")
 
-        us.settings["App ID"] = "123"
+        us.settings.app_id = "123"
         us.save_to_file()
         with open(os.path.abspath(__file__ + "/../../user_settings.json"), "r") as f:
             saved = json.load(f)
@@ -58,19 +58,19 @@ class UserSettingsTest(unittest.TestCase):
 
     def test_load_from_file_overwrites_settings(self) -> None:
         us = UserSettings()
-        self.assertTrue(us.settings["App ID"] == "")
+        self.assertTrue(us.settings.app_id == "")
 
-        us.settings["App ID"] = "123"
+        us.settings.app_id = "123"
         us.save_to_file()
-        self.assertEqual(us.settings["App ID"], "123")
-        us.settings["App ID"] = "456"
-        self.assertEqual(us.settings["App ID"], "456")
+        self.assertEqual(us.settings.app_id, "123")
+        us.settings.app_id = "456"
+        self.assertEqual(us.settings.app_id, "456")
         us.load_from_file()
-        self.assertEqual(us.settings["App ID"], "123")
+        self.assertEqual(us.settings.app_id, "123")
 
     def test_load_from_file_creates_file_if_not_exists(self) -> None:
         us = UserSettings()
-        self.assertTrue(us.settings["App ID"] == "")
+        self.assertTrue(us.settings.app_id == "")
         self.assertTrue(os.path.exists(us.file_loc))
         os.remove(us.file_loc)
 
