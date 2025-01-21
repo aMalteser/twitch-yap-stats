12f3eddf068e73deb4e46bb50f35353c75594dd8
Mateusz Krawczynski <matikrawczpl@gmail.com>
2025-01-21 12:49:29 +0000

    refactor prompt
    - split `print_options` into `all`, `common`, `quit`,
    - rename `user_input` to `user_option` in prompt loops
    - rename `print_option` to `handle_user_input`, it was quite confusing
    - remove duplicate code, with checks to make sure the right things are
    displayed/accounted for

--- src/user_prompt.py
+++ src/user_prompt.py
@@ -7,51 +7,76 @@ def clear() -> None:
     os.system("cls" if os.name == "nt" else "clear")
 
 
-def print_options() -> None:
+def print_options_common() -> None:
     settings = UserSettings().settings
     print(f"1. {'Set' if settings.app_id == '' else 'Change'} App ID")
     print(f"2. {'Set' if settings.app_secret == '' else 'Change'} App Secret")
+
+
+def print_options_all() -> None:
+    settings = UserSettings().settings
+    print_options_common()
+
     if settings.target_channel == "":
         print("3. Set Target Channel")
     else:
         print(f"3. Change Target Channel ({settings.target_channel})")
+
     print(
-        f"4. Toggle Excluded User (Currently Excluded: {
-            list(settings.excluded_users)})"
+        f"4. Toggle Excluded User (Currently Excluded: {list(settings.excluded_users)})"
     )
     print(
         f"5. Toggle Console Logging (Currently {
             'Enabled' if settings.logging else 'Disabled'
         })"
     )
-    print(f"6. Change Padding (Currently {settings.padding})", end="\n\n")
-    print("r. Run Bot")
+    print(f"6. Change Padding (Currently {settings.padding})")
+    print_options_quit()
+
+
+def print_options_quit(type: str = "App") -> None:
+    print()
+    print(f"r. Run {type}")
     print("q. Quit", end="\n\n")
 
 
-def print_option(option: str) -> None:
+def print_options_server() -> None:
+    print_options_common()
+    print_options_quit("Server")
+
+
+def handle_user_input(option: str, from_server: bool) -> None:
     clear()
     print("Press enter to cancel")
 
-    settings = UserSettings().settings
-    u: str = ""
-    match option:
-        case "1":
-            u = input("Enter App ID: ")
-        case "2":
-            u = input("Enter App Secret: ")
-        case "3":
-            print(f"Current Target Channel: {settings.target_channel}")
-            u = input("New Target Channel: ")
-        case "4":
-            print(f"Current Excluded Users: {list(settings.excluded_users)}")
-            u = input("Enter User to Toggle: ")
-        case "6":
-            u = input("Enter Padding: ")
+    user_input = get_user_input(option, from_server)
 
-    if u == "" and option != "5":
+    # Padding option will always return ""
+    # should only be changed when not from server settings
+    if user_input == "" and (option != "5" and not from_server):
         return
-    update_option(option, u)
+
+    # No need to check again if from server
+    update_option(option, user_input)
+
+
+def get_user_input(option: str, from_server: bool) -> str:
+    settings = UserSettings().settings
+    match (option, from_server):
+        case "1", _:
+            return input("Enter App ID: ")
+        case "2", _:
+            return input("Enter App Secret: ")
+        case "3", False:
+            print(f"Current Target Channel: {settings.target_channel}")
+            return input("New Target Channel: ")
+        case "4", False:
+            print(f"Current Excluded Users: {list(settings.excluded_users)}")
+            return input("Enter User to Toggle: ")
+        case "6", False:
+            return input("Enter Padding: ")
+
+    return ""
 
 
 def update_option(option: str, new_val: str):
@@ -82,57 +107,25 @@ def update_option(option: str, new_val: str):
 def prompt_loop() -> None:
     while True:
         clear()
-        print_options()
+        print_options_all()
 
-        user_input = input("Enter option: ").lower()
-        if user_input == "r":
+        user_option = input("Enter option: ").lower()
+        if user_option == "r":
             break
-        if user_input == "q":
+        if user_option == "q":
             exit()
-        print_option(user_input)
+        handle_user_input(user_option, False)
 
     clear()
-
-
-def print_options_server() -> None:
-    settings = UserSettings().settings
-    print(f"1. {'Set' if settings.app_id == '' else 'Change'} App ID")
-    print(
-        f"2. {'Set' if settings.app_secret ==
-          '' else 'Change'} App Secret\n"
-    )
-    print("r. Run server")
-    print("q. Quit\n")
-
-
-def handle_option_server(option: str) -> None:
-    clear()
-    print("Press enter to cancel")
-
-    user_settings = UserSettings()
-    settings = user_settings.settings.copy()
-    u: str = ""
-    match option:
-        case "1":
-            u = input("Enter App ID: ")
-            settings.app_id = u
-        case "2":
-            u = input("Enter App Secret: ")
-            settings.app_secret = u
-
-    if u == "":
-        return
-    user_settings.settings = settings
-    user_settings.save_to_file()
 
 
 def server_prompt_loop() -> None:
     while True:
         print_options_server()
 
-        user_input = input("Enter option: ").lower()
-        if user_input == "r":
+        user_option = input("Enter option: ").lower()
+        if user_option == "r":
             break
-        if user_input == "q":
+        if user_option == "q":
             exit()
-        handle_option_server(user_input)
+        handle_user_input(user_option, True)
